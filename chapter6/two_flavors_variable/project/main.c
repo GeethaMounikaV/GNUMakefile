85740fe05ae954deed4550ae883e47efbe0a7928
Geetha-Mounika-Velagaleti <geethamounika.velagaleti@vconnectech.in>
2025-09-22 12:08:12 +0530

    two flavours variable example

---
+++ chapter6/two_flavors_variable/project/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Main program running...\n");
+    util_function();
+    return 0;
+}
+
---
+++ chapter6/two_flavors_variable/project/utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "utils.h"
+
+void util_function() {
+    printf("Utility function called!\n");
+}
+
