f6fc262b37b38a2fdcb18dacb8da21b4a86171ac
Geetha-Mounika-Velagaleti <geethamounika.velagaleti@vconnectech.in>
2025-09-26 18:04:19 +0530

    Variables from the environment

---
+++ chapter6/envnment_var/project/include/goodbye.h
@@ -0,0 +1,7 @@
+#ifndef GOODBYE_H
+#define GOODBYE_H
+
+void say_goodbye(void);
+
+#endif
+
---
+++ chapter6/envnment_var/project/src/goodbye.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "goodbye.h"
+
+void say_goodbye(void) {
+    printf("Goodbye from goodbye.c!\n");
+}
+
