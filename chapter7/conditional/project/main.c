995ae1e6e3f53a7976df2e03f53f48d3ea736372
Geetha-Mounika-Velagaleti <geethamounika.velagaleti@vconnectech.in>
2025-09-29 13:01:36 +0530

    Conditional parts of Makefile example

---
+++ chapter7/conditional/project/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math.h"
+
+int main(void) {
+    printf("Main program running...\n");
+    print_sum(5, 7);
+    return 0;
+}
+
---
+++ chapter7/conditional/project/math.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "math.h"
+
+void print_sum(int a, int b) {
+    printf("Sum = %d\n", a + b);
+}
+
