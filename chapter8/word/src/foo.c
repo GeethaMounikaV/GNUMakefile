c10669d2d4ad886f5866743788d94b442f7734fe
Geetha-Mounika-Velagaleti <geethamounika.velagaleti@vconnectech.in>
2025-10-07 12:35:06 +0530

    sort word function call syntax

---
+++ chapter8/word/src/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void foo_func(void) {
+    printf("Hello from foo!\n");
+}
+
---
+++ chapter8/word/src/foo.h
@@ -0,0 +1,7 @@
+#ifndef FOO_H
+#define FOO_H
+
+void foo_func(void);
+
+#endif
+
